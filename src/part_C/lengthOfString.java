af5042ff7ab569077bf0d7c5228a60d8334326a5
Lisa Princesse <prilisa22@gmail.com>
2019-01-18 22:14:16 +0000

    added a lengthOfString class

---
+++ src/part_C/lengthOfString.java
@@ -0,0 +1,14 @@
+package part_C;
+
+public class lengthOfString {
+
+	public lengthOfString() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
